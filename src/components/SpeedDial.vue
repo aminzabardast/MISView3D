087c834c7505f61404c0db8a3a380fb77efd6196
Amin <aminzabardast@gmail.com>
2019-07-22 22:27:56 +0300

    Adding go to top button

--- src/components/SpeedDial.vue
+++ src/components/SpeedDial.vue
@@ -1,6 +1,6 @@
 <template>
     <div id="speed-dial">
-        <v-speed-dial bottom right v-model="fab">
+        <v-speed-dial bottom right open-on-hover v-model="fab">
             <template v-slot:activator>
                 <v-btn v-model="fab" fab>
                     <v-icon>more_vert</v-icon>
@@ -15,17 +15,28 @@
                 </template>
                 <span>{{invertThemeTooltipText}}</span>
             </v-tooltip>
+            <v-tooltip left v-if="showToTopBtn">
+                <template v-slot:activator="{ on }">
+                    <v-btn fab small v-on="on" @click="goToTop">
+                        <v-icon>keyboard_arrow_up</v-icon>
+                    </v-btn>
+                </template>
+                <span>Go To Top</span>
+            </v-tooltip>
         </v-speed-dial>
     </div>
 </template>
 
 <script>
+    import * as $ from 'jquery'
+
     export default {
         name: "SpeedDial",
         data () {
             return {
                 fab: false,
                 darkTheme: true,
+                showToTopBtn: true,
                 invertThemeTooltipText: 'Light Theme'
             }
         },
@@ -33,12 +44,24 @@
             invertTheme () {
                 this.$emit('invertTheme');
                 this.darkTheme = !this.darkTheme;
+            },
+            goToTop () {
+                $("html, body").animate({ scrollTop: 0 }, "fast");
+            },
+            handleScroll () {
+                this.showToTopBtn = $(window).scrollTop() > 100;
             }
         },
         watch: {
             darkTheme () {
                 this.darkTheme ? this.invertThemeTooltipText = 'Light Theme' : this.invertThemeTooltipText = 'Dark Theme'
             }
+        },
+        created () {
+            window.addEventListener('scroll', this.handleScroll);
+        },
+        destroyed () {
+            window.removeEventListener('scroll', this.handleScroll);
         }
     }
 </script>
